1f624113bd4312e432000547010550c66b9ec853
David <David@DESKTOP-ONIORCL>
2019-03-26 21:24:53 -0300

    Commit inicial

---
+++ contador-de-palabras/contador-de-palabras/Parrafo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contador_de_palabras
+{
+    class Parrafo
+    {
+        private String palabras;
+        public void setParrafo(String n)
+        {
+
+            palabras = n;
+
+        }
+        public String getParrafo()
+        {
+
+            return palabras;
+
+        }
+
+        public static implicit operator string(Parrafo v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ contador-de-palabras/contador-de-palabras/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contador_de_palabras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //List<String> parrafo = new List<string>;
+
+            Parrafo p = new Parrafo();
+
+            Console.WriteLine("Escribi un parrafo\n");
+
+            p.setParrafo(Console.ReadLine());
+
+            String parrafob = "";
+
+            // String palabras = "";
+
+            parrafob = p.ToString();
+
+
+            Console.WriteLine(parrafob);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
