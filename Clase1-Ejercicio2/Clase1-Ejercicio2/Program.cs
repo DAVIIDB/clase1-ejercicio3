57397a7ae814cf5eb66a2fc5436fde8f245546fd
David <David@DESKTOP-ONIORCL>
2019-03-27 00:04:30 -0300

    Clase 1 Ejercicio 2 final

--- Clase1-Ejercicio2/Clase1-Ejercicio2/Program.cs
+++ Clase1-Ejercicio2/Clase1-Ejercicio2/Program.cs
@@ -10,6 +10,33 @@ namespace Clase1_Ejercicio2
     {
         static void Main(string[] args)
         {
+            Dictionary<String, int> gente = new Dictionary<String, int>();
+
+            gente.Add("Juan", 55423412);
+            gente.Add("Ernesto", 56985623);
+            gente.Add("Mariana", 54787451);
+
+
+            if (gente.ContainsKey("Juan"))
+                Console.WriteLine("El valor es" + gente["Juan"]);
+            if (gente.ContainsKey("Pedro"))
+                Console.WriteLine("El valor de Pedro es:" + gente["Pedro"]);
+            else
+                Console.WriteLine("El nombre no existe");
+            foreach (var item in gente)
+            {
+                Console.WriteLine(item);
+            }
+            gente.Remove("Mariana");
+
+            gente.Add("Mariana", 58251425);
+
+            Console.WriteLine("El nuevo valor de Mariana es" + gente["Mariana"]);
+
+
+
+
+
         }
     }
 }
